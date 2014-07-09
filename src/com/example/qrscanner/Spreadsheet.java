c6132ce070225f1e34cdfba14bece333e851e9a0
Samiya Akhtar <samiyaakhtar7@gmail.com>
2014-07-08 23:09:23 -0400

    Adding spreadsheet dependencies

---
+++ src/com/example/qrscanner/Spreadsheet.java
@@ -0,0 +1,16 @@
+package com.example.qrscanner;
+import com.google.gdata.client.authn.oauth.*;
+import com.google.gdata.client.docs.*;
+import com.google.gdata.data.*;
+import com.google.gdata.data.batch.*;
+import com.google.gdata.data.docs.*;
+import com.google.gdata.util.*;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+
+public class Spreadsheet {
+
+}
