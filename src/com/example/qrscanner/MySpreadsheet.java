7eb92aa3e2120e440bce1da94d6710c061d9f4ff
Samiya Akhtar <samiyaakhtar7@gmail.com>
2014-07-09 23:28:25 -0400

    Using OAuth2.0 to read spreaadsheet

--- src/com/example/qrscanner/MySpreadsheet.java
+++ src/com/example/qrscanner/MySpreadsheet.java
@@ -3,6 +3,7 @@ import android.app.Activity;
 import android.os.AsyncTask;
 
 import com.google.gdata.client.authn.oauth.*;
+import com.google.gdata.client.authn.oauth.OAuthParameters.OAuthType;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 import com.google.gdata.data.*;
 import com.google.gdata.data.batch.*;
@@ -11,6 +12,14 @@ import com.google.gdata.data.spreadsheet.ListFeed;
 import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
 import com.google.gdata.util.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.gdata.client.authn.oauth.GoogleOAuthParameters; 
+import com.google.gdata.client.docs.*;
+import com.google.gdata.client.authn.oauth.*;
 
 import java.io.IOException;
 import java.net.*;
@@ -19,15 +28,13 @@ import java.util.*;
 
 public class MySpreadsheet {
 	
-	private String GOOGLE_ACCOUNT_USERNAME = "samiyaakhtar7@gmail.com"; 
-	private String GOOGLE_ACCOUNT_PASSWORD = "<censored>"; 
 	
 	private MainActivity m_activity;
 
-	private String SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1yd-R2YhoznZBWlPeEY-ZU9tcrENYpxIA3-VeYsFmIEk/edit#gid=479264020";
+	public static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 	
 	private class checkSpreadsheet extends AsyncTask<String, Void, Boolean>{
-
+		
 		@Override
 		protected Boolean doInBackground(String... params) {
 			// TODO Auto-generated method stub
@@ -35,17 +42,37 @@ public class MySpreadsheet {
 			{
 		        System.out.println("Hi1");
 		        
-				SpreadsheetService client = new SpreadsheetService("Print Google Spreadsheet Demo");
+
+	        GoogleOAuthParameters oauthParameters = new GoogleOAuthParameters();
+	        oauthParameters.setOAuthConsumerKey (Config.CLIENT_ID);
+	        oauthParameters.setOAuthConsumerSecret (Config.CLIENT_SECRET);
+	        oauthParameters.setOAuthType (OAuthType.TWO_LEGGED_OAUTH);
+	        oauthParameters.setScope ("https://spreadsheets.google.com/feeds/");
+
+			SpreadsheetService client = new SpreadsheetService("Print Google Spreadsheet Demo");
+			client.useSsl ();
+			client.setOAuthCredentials (oauthParameters, new OAuthHmacSha1Signer ());
+
+
 				if( client != null ){
-					client.setUserCredentials(GOOGLE_ACCOUNT_USERNAME, GOOGLE_ACCOUNT_PASSWORD);
+					client.setUserCredentials(Config.GOOGLE_ACCOUNT_USERNAME, Config.GOOGLE_ACCOUNT_PASSWORD);
 			        
-					URL metafeedUrl = new URL(SPREADSHEET_URL);
-			        SpreadsheetFeed feed = client.getFeed(metafeedUrl, SpreadsheetFeed.class);
-			        
-			        List<SpreadsheetEntry> spreadsheets = feed.getEntries();
-			        for (SpreadsheetEntry service : spreadsheets) {             
-			            System.out.println(service.getTitle().getPlainText());
-			       }
+					URL metafeedUrl = new URL(Config.SPREADSHEET_URL);
+					
+					SpreadsheetEntry spreadsheet = client.getEntry(metafeedUrl,SpreadsheetEntry.class);
+			        URL listFeedUrl = spreadsheet.getWorksheets().get(0).getListFeedUrl();
+
+			        // Print entries
+			        ListFeed feed = client.getFeed(listFeedUrl, ListFeed.class);
+
+			        for (ListEntry entry : feed.getEntries()) {
+			            System.out.println("new row");
+			            for (String tag : entry.getCustomElements().getTags()) {
+			                System.out.println("     " + tag + ": "
+			                        + entry.getCustomElements().getValue(tag));
+
+			            }
+			        }
 			        
 			        System.out.println("YOOOO");
 				}
