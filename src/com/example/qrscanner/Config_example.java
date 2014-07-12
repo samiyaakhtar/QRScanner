51d475866c001937f76dca43dd5d3b88ba74f6f7
Samiya Akhtar <samiyaakhtar7@gmail.com>
2014-07-12 12:43:31 -0400

    Config class example - substitute with values

---
+++ src/com/example/qrscanner/Config_example.java
@@ -0,0 +1,12 @@
+package com.example.qrscanner;
+
+public class Config_example {
+
+	public static String GOOGLE_ACCOUNT_USERNAME = "example@gmail.com"; 
+	public static String GOOGLE_ACCOUNT_PASSWORD = "<your_password>"; 
+	public static String CLIENT_ID = "<your_client_id>";
+	public static String CLIENT_SECRET = "<client_secret_id>";
+	public static String SPREADSHEET_URL = "<spreadsheet_url>";
+	public static String SPREADSHEET_PUBLIC_URL = "<published_spreadsheet_url>";
+
+}
